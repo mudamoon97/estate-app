437e339915491a28e17de347c60aa75507148d9e
mudamoon97 <53881669+mudamoon97@users.noreply.github.com>
2020-06-24 02:17:10 +0100

    added models for login/register

---
+++ src/EstateApp.Web/Models/LoginModel.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EstateApp.Web.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
---
+++ src/EstateApp.Web/Models/RegisterModel.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace EstateApp.Web.Models
+{
+    public class RegisterModel
+    {
+        [DisplayName("Name")]
+        [Required]
+        public string FullName { get; set; }
+
+
+
+        [DisplayName("Email Address")]
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DisplayName("Confirm Password")]
+        [Required]
+        [Compare(nameof(Password))]      
+        public string ConfirmPassword { get; set; }
+    }
+}
